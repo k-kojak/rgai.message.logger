646bc978beb70f15b4be8de40d72ee709bacf760
Tamas Kojedzinszky <tamas.kojedzinszky@gmail.com>
2013-11-06 17:17:51 +0100

    Adding setters, removing compareTo.

--- src/hu/uszeged/inf/rgai/messagelog/beans/MessageListElement.java
+++ src/hu/uszeged/inf/rgai/messagelog/beans/MessageListElement.java
@@ -11,7 +11,7 @@ import java.util.Date;
  * 
  * @author Tamas Kojedzinszky
  */
-public class MessageListElement implements Comparable<MessageListElement> {
+public class MessageListElement /*implements Comparable<MessageListElement>*/ {
   
   protected String id;
   protected boolean seen;
@@ -143,6 +143,18 @@ public class MessageListElement implements Comparable<MessageListElement> {
     this.seen = seen;
   }
 
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public void setSubTitle(String subTitle) {
+    this.subTitle = subTitle;
+  }
+
+  public void setDate(Date date) {
+    this.date = date;
+  }
+
   @Override
   public String toString() {
     return "MessageListElement{" + "id=" + id + ", seen=" + seen + ", title=" + title + ", subTitle=" + subTitle + ", from=" + from + ", date=" + date + ", messageType=" + messageType + '}';
@@ -157,7 +169,7 @@ public class MessageListElement implements Comparable<MessageListElement> {
       return false;
     }
     final MessageListElement other = (MessageListElement) obj;
-    if (this.id != other.id) {
+    if (this.id.equals(other.id)) {
       return false;
     }
 //    if (this.seen != other.seen) {
@@ -172,19 +184,23 @@ public class MessageListElement implements Comparable<MessageListElement> {
     if (this.from != other.from && (this.from == null || !this.from.equals(other.from))) {
       return false;
     }
-    if (this.date != other.date && (this.date == null || !this.date.equals(other.date))) {
-      return false;
-    }
+//    if (this.date != other.date && (this.date == null || !this.date.equals(other.date))) {
+//      return false;
+//    }
     if (this.messageType != other.messageType) {
       return false;
     }
     return true;
   }
 
-  @Override
-  public int compareTo(MessageListElement o) {
-    return -1 * this.date.compareTo(o.date);
-  }
+//  @Override
+//  public int compareTo(MessageListElement o) {
+//    if (this.equals(o)) {
+//      return 0;
+//    } else {
+//      return -1 * this.date.compareTo(o.date);
+//    }
+//  }
 
   
   
